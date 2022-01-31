5aaf0b3b4867c8967f97f18a895b7234c5a09339
Long Do <longdo@dobtech.vn>
2022-01-31 17:34:45 +0700

    refactor using static method

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,8 @@
-import { WinAnalysis } from './analyzers/WinAnalysis';
-import { CsvFileReader } from './CsvFileReader';
 import { MatchReader } from './MatchReader';
-import { ConsoleReport } from './reportTargets/ConsoleReport';
 import { Summary } from './Summary';
 
-const csvFileReader = new CsvFileReader('football.csv');
-const matchReader = new MatchReader(csvFileReader);
-matchReader.load();
-
-// const summary = new Summary(new WinAnalysis('Man United'), new ConsoleReport());
+const matchReader = MatchReader.fromCsv('football.csv');
 const summary = Summary.winsAnalysisWithHtmlReport('Man United');
 
+matchReader.load();
 summary.buildAndPrintReport(matchReader.matches);
